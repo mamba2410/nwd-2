9d30e97bec5ce6417c7148fbe600b82ae40e6051
mamba2410 <callummcgregor54@gmail.com>
2020-01-14 20:23:42 +0000

    Added C++

---
+++ languages/cpp/specifics.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+language_home="$1/languages/cpp"
+project_name="$2"
+do_init=$3
+
+cp $language_home/Makefile ./bin/Makefile
+cp $language_home/build_number.mak ./bin/metadata/build_number.mak
+
+project_name_upper=$(echo $project_name | tr '[:lower:]' '[:upper:]')
+project_name_upper=$(echo $project_name_upper | sed 's/[^[:alpha:]]/\_/g')
+
+if [ $do_init = 1 ]; then
+	cp $language_home/init.cpp ./src/$project_name.cpp
+	cp $language_home/init.h ./include/$project_name.h
+
+	sed -i "s/PROJECT_NAME/$project_name/g" ./src/$project_name.cpp
+	sed -i "s/PROJECT_NAME/$project_name_upper/g" ./include/$project_name.h
+
+fi
